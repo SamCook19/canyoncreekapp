a74f7d6cbdf5df039e91c52affc206cd94f6fefb
Sam Cook <sam.cook@suusuccess.org>
2021-01-15 13:01:12 -0700

    fixed font bug

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Layout from './Layout';
-
+import '../../static/assets/fonts/stellar-light.woff';
 
 
 export default class App extends Component {
