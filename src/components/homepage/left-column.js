e18723d3f32a55c735f87ef597c0617a6ea5c723
Sam Cook <sam.cook@suusuccess.org>
2021-03-11 13:24:27 -0700

    added alt images for hovering, social media spacing

--- src/components/homepage/left-column.js
+++ src/components/homepage/left-column.js
@@ -8,19 +8,29 @@ class LeftColumn extends Component {
                <div className='quote-slide-show'>
                 Cycling quotes 
                 </div>
+                <div className='social-media-container'>
                 <div className='social-media-icons'>
+                <div className='facebook-home'>
                 <a className='facebook-icon' href='https://www.facebook.com/CCreekServices/'>
                 <img  alt="ccs facebook" src="/assets/images/facebook.png"/>
                 </a>
+                </div>
+                <div className='instagram-home'>
                 <a className='instagram-icon' href='https://www.instagram.com/ccreekservices/?hl=en'>
                 <img  alt="ccs instagram" src="/assets/images/instagram.png"/>
                 </a>
+                </div>
+                <div className='linkedin-home'>
                 <a className='linkedin-icon' href='https://www.linkedin.com/company/canyon-creek-services/'>
                 <img  alt="ccs linkedin" src="/assets/images/linkedin.png"/>
                 </a>
+                </div>
+                <div className='twitter-home'>
                 <a className='twitter-icon' href='https://twitter.com/ccreekservices?lang=en'>
                 <img  alt="ccs twitter" src="/assets/images/twitter.png"/>
                 </a>
+                </div>
+                </div>
                 </div>
             </div>
         );
