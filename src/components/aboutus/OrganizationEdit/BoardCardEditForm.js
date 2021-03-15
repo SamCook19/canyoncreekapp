0562e3d834fcadf96bf2c33021caeea1456e54c2
Sam Cook <sam.cook@suusuccess.org>
2021-03-15 12:55:33 -0600

    began work on leadership card edit

---
+++ src/components/aboutus/OrganizationEdit/BoardCardEditForm.js
@@ -0,0 +1,177 @@
+import React, { Component } from 'react';
+import {Container, Row, Col, FormGroup, Label, Input, Button} from 'reactstrap';
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import firebase from "firebase";
+import { v4 as uuidv4 } from 'uuid'
+
+const db = firebase.default.firestore()
+const storageRef = firebase.storage()
+
+class BoardCardEdit extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state={
+            article: {
+                boardname: "",
+                boardSubtitle: "",
+                featuredImage: ''
+            }
+        }
+    }
+
+    modules = {
+        toolbar: {
+            container: [
+                [{'header': '1'}, {'header': '2'}, {'font': []}],
+                [{size: []}],
+                ['bold','italic', 'underline', 'strike', 'blockquote'],
+                [{'list': 'ordered'}, {'list': 'bullet'},
+                    {'indent': '-1'}, {'indent': '+1'}],
+                    ['link', 'image'],
+                    ['clean'], ['code-block']
+            ],
+        },
+        clipboard: {
+            matchvisual: false,
+        },
+    }
+
+    formats = [
+        'header',
+        'font',
+        'size',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'blockquote',
+        'list',
+        'bullet',
+        'indent',
+        'link',
+        'image',
+        'video',
+        'code-block'
+    ]
+
+    onChangeCardSub = (value) => {
+        this.setState({
+            article: {
+                ...this.state.article,
+                boardSubtitle: value
+            }
+        })
+    }
+
+    onChangeCardName = (value) => {
+        this.setState({
+            article: {
+                ...this.state.article,
+                boardname: value
+            }
+        })
+    }
+
+
+    submitEdit = (aid) => {
+        const article = this.state.article
+        db.collection("OrganizationalLeadershipBD")
+        .doc(aid)
+            .update(
+                article
+                )
+            .then( res => {
+                console.log(res)
+            } )
+            .catch( err => console.log(err))
+    }
+
+    uploadImageCallBack = (e) => {
+        return new Promise(async(resolve, reject) => {
+            const file = e.target.files[0]
+            const fileName = uuidv4()
+            storageRef.ref().child("Board/" + fileName).put(file)
+            .then( async snapshot => {
+                
+                const downloadURL = await storageRef.ref().child("Board/" +fileName).getDownloadURL()
+
+                resolve({
+                    success: true,
+                    data: {link: downloadURL}
+                })
+            })
+        })
+    }
+
+
+    render() {
+        return (
+            <Container>
+            <Row>
+                <Col >
+                <h2 className='SectionTitle'>Edit Card</h2>
+                    <FormGroup className='TitleForm'>
+                        <Label className='TitleLabel'>
+                            <span style={{color: "white"}}>Title</span>
+                        </Label>
+                        <input type ='text' className='editArticleTitle' placeholder=''
+                        onChange={(e) => this.onChangeCardName(e.target.value)}
+                        value={this.state.article.title}/>
+                    </FormGroup>            
+                </Col>
+                <div className='edit-article-container'>
+                    <FormGroup className='left-column'>
+                        <ReactQuill className='edit-quill'
+                            ref0={(el) => this.quill = el}
+                            value={this.state.article.content}
+                            onChange={(e) => this.onChangeCardSub(e)}
+                            theme='snow'
+                            modules={this.modules}
+                            formats={this.formats}
+                        />
+                        
+
+                        
+                        
+                        <FormGroup className='edit-status-button'>
+                                <Button className='edit-submit'
+                                onClick={(e) => this.submitEdit()}>
+                                    Submit
+                                </Button> 
+                            </FormGroup>
+                            
+                        </FormGroup>
+                <Col className='right-column'>
+                            
+                            <FormGroup className='edit-featured-image'> Featured Image
+                                <Input type="file" accept='image/*' className="image-uploader"
+                                onChange={async (e) => {
+                                    const uploadState = await this.uploadImageCallBack(e)
+                                    if(uploadState.success) {
+                                        this.setState({
+                                            hasFeaturedImage: true,
+                                            article: {
+                                                ...this.state.article,
+                                                featuredImage: uploadState.data.link
+                                            }
+                                        })
+                                    }
+                                }}> 
+                                </Input>
+                                <div className='image'>
+                                {
+                                    this.state.hasFeaturedImage ?
+                                        <img src={this.state.article.featuredImage} /> : ''
+                                }</div>
+                            </FormGroup>
+                </Col>
+                </div>
+            </Row>
+            </Container>
+        );
+    }
+}
+
+export default BoardCardEdit;
