1a714d13665003b3ff92ae9545446eb9cf66e401
Sam Cook <sam.cook@suusuccess.org>
2021-03-19 18:27:47 -0600

    began working on carousel styles

--- src/components/homepage/right-carousel.js
+++ src/components/homepage/right-carousel.js
@@ -11,56 +11,56 @@ function ControlledCarousel() {
 
 
     return (
-      <Carousel activeIndex={index} onSelect={handleSelect} indicators>
+      <Carousel activeIndex={index} onSelect={handleSelect} indicators className='right-side-carousel'>
         <Carousel.Item>
-        <Carousel.Caption>
+        <Carousel.Caption className='carousel-caption'>
           <div className="carousel-caption">
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </div>
-          </Carousel.Caption>
+          </Carousel.Caption >
           <img
-            className="d-block w-100"
+            className="d-block w-100 carouselimg"
             src="https://picsum.photos/960/300"
             alt="First slide"
             style = {{
                 maxHeight: 450,
                 height: 450,
-                width:300
+                width:40
             }}
           />
           
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="d-block w-100 carouselimg"
             src="https://picsum.photos/960/300"
             alt="Second slide"
             style = {{
                 maxHeight: 450,
                 height: 450,
-                width: 100
+                width:40
             }}
           />
   
-          <Carousel.Caption>
+          <Carousel.Caption className='carousel-caption'>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="d-block w-100 carouselimg"
             src="https://picsum.photos/960/300"
             alt="Third slide"
             style = {{
                 maxHeight: 450,
                 height: 450,
-                width:100
+                width:40
             }}
           />
   
-          <Carousel.Caption>
+          <Carousel.Caption className='carousel-caption'>
             <h3>Third slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
