436a2782269ba6f1fac25c7a13cfdd40c4783f4b
Sam Cook <sam.cook@suusuccess.org>
2021-04-02 13:13:53 -0600

    deployment work

--- src/components/homepage/HeroImage.js
+++ src/components/homepage/HeroImage.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { CardImg, CardTitle } from 'reactstrap';
+import Image from 'react-bootstrap/Image';
 
 const HeroImage = (props) => {
         return (
             <div className='hero-image-container'>
             <div className="hero-image-img">
-            <CardImg
+            <Image
             top
             src={props.data.featuredImage}
             alt="Card Image"
             className="CardImage"
+            fluid
             />
             </div>
             </div>
