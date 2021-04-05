266c4bfc645f09dbec169833cd7bc96740382f97
Sam Cook <sam.cook@suusuccess.org>
2021-04-05 14:09:37 -0600

    mobile scroll issue

--- src/components/logo/pagelogo.js
+++ src/components/logo/pagelogo.js
@@ -7,7 +7,7 @@ class PageLogo extends Component {
        
         return (
             <div className="logo-main">
-                <Link to="/" className='logo-container'>
+                <Link to="/">
                     <img alt="ccs logo home" src="/assets/images/ccslogo.png"/>
                 </Link>
             </div>
