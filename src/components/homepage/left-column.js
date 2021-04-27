5c2108f699fee8a388b9dc4d7a15a26445e8c7e7
Sam Cook <sam.cook@suusuccess.org>
2021-04-27 13:06:18 -0600

    worked on adjusting images for mobile

--- src/components/homepage/left-column.js
+++ src/components/homepage/left-column.js
@@ -14,8 +14,13 @@ class LeftColumn extends Component {
               <div className='quote-slide-show'>
                 <QuoteCarousel />
                 </div>
+                <div className='homepage-socialmedia-subscriptionform'>
                 <div className='social-media-container'>
                   <HomepageSocialMedia />
+                </div>
+                <div className='sub-form'>
+                <a href='https://canyoncreekservices.salsalabs.org/SubscriptionForm'>Join Our Email List</a>
+                </div>
                 </div>
             </div>
             
